85ecba66a60792bddf5ac94696e4d6c9f27664f5
fikipraktek <fikiantopraktek@gmail.com>
2025-10-12 16:46:02 +0700

    Menambahkan file Week 1 dan konfigurasi

---
+++ praktikum/week1-setup-hello-pos/src/main/java/com/upb/agripos/HelloOOP.java
@@ -0,0 +1,22 @@
+package main.java.com.upb.agripos;
+
+    
+class Student {
+    String nim;
+    String name;
+    Student(String nim, String name) {
+        this.nim = nim;
+        this.name = name;
+    }
+
+    void introduce() {
+        System.out.println("Hello World, I am " + name + " - " + nim);
+    }
+}
+
+public class HelloOOP {
+    public static void main(String[] args) {
+        Student s = new Student("240202899", "Fiki Anto");
+        s.introduce();
+    }
+}
---
+++ praktikum/week1-setup-hello-pos/src/main/java/com/upb/agripos/HelloProcedural.java
@@ -0,0 +1,12 @@
+package main.java.com.upb.agripos;
+
+
+
+public class HelloProcedural {
+    public static void main(String[] args) {
+        String nim = "240202899";
+        String name = "Fiki Antoc";
+
+        System.out.println("Hello World, I am " + name + " - " + nim);
+    }
+}
